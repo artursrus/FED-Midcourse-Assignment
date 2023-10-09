0180b92a5d92ed5d61c0bead33a4336ce752f738
artursrus <arturs.ruskulis1@gmail.com>
2023-10-09 20:22:18 +0100

    add new page which displays workouts

--- assets/js/modules/api.js
+++ assets/js/modules/api.js
@@ -1,5 +1,5 @@
 // Api KEY = 0hmDrXG1OUselhJqNDT5Jg==31C3PNP5j2D66iM2
-
+const displayExercises = document.querySelector('#displayExercises')
 const getExercises = async (back) => {
     const urlAPI = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=10`;
     const keyAPI = {
@@ -11,8 +11,35 @@ const getExercises = async (back) => {
     };
 
     const response = await fetch(urlAPI, keyAPI);
-    const result = response.text();
-    console.log(result);
+    const result = await response.json();
+
+    //Checks the length of an object
+    if (result.length > 0) {
+        
+        //For each object displays name in the html
+        result.forEach((exercise, index ) => {
+            console.log(exercise.name);
+            //Creating html elements 
+            const exerciseDiv = document.createElement('div')
+            const exerciseGif = document.createElement('img')
+
+            //Putting API information into html elements
+            exerciseDiv.innerHTML += exercise.name;
+            exerciseGif.src = exercise.gifUrl;
+
+            //Appending so that image is in the div container
+            displayExercises.appendChild(exerciseDiv)
+            displayExercises.appendChild(exerciseGif)
+            exerciseDiv.appendChild(exerciseGif)
+
+            exerciseDiv.classList.add('exercises')
+            exerciseDiv.setAttribute('id', index)
+            console.log(index)
+        });
+
+        console.log(result)
+
+    }
 
     
 }
